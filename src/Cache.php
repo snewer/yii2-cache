f0728855300ed4dad01881dc9d56f2f3da74f51c
Кирилл <snewer0@gmail.com>
2018-04-10 01:55:42 +0400

    initial commit.

---
+++ src/Cache.php
@@ -0,0 +1,107 @@
+<?php
+
+namespace snewer\cache;
+
+use Yii;
+use yii\base\Component;
+use Psr\SimpleCache\CacheInterface;
+
+/**
+ * Class Cache
+ * @package snewer\cache
+ * @see https://www.php-fig.org/psr/psr-16/
+ */
+class Cache extends Component implements CacheInterface
+{
+
+    /**
+     * @var string
+     */
+    public $baseCache = 'cache';
+
+    /**
+     * @return \yii\caching\Cache
+     */
+    protected function _getBaseCache()
+    {
+        return Yii::$app->get($this->baseCache, true);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function get($key, $default = null)
+    {
+        $value = $this->_getBaseCache()->get($key);
+        return ($value === false) ? $default : $value;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function set($key, $value, $ttl = null)
+    {
+        return $this->_getBaseCache()->set($key, $value, $ttl);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function delete($key)
+    {
+        return $this->_getBaseCache()->delete($key);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function has($key)
+    {
+        return $this->_getBaseCache()->exists($key);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getMultiple($keys, $default = null)
+    {
+        $result = [];
+        foreach ($keys as $key) {
+            $result[$key] = $this->get($key, $default);
+        }
+        return $result;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function setMultiple($values, $ttl = null)
+    {
+        $success = true;
+        foreach ($values as $key => $value) {
+            $success = $success && $this->set($key, $value, $ttl);
+        }
+        return $success;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function deleteMultiple($keys)
+    {
+        $success = true;
+        foreach ($keys as $key) {
+            $success = $success && $this->delete($key);
+        }
+        return $success;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function clear()
+    {
+        return $this->_getBaseCache()->flush();
+    }
+
+}
